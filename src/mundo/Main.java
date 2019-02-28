3445bb000f18f3cbb75fb09d4908fda2cf26238b
David Eduardo Saavedra Hernandez <de.saavedra@uniandes.edu.co>
2019-02-27 23:19:07 -0500

    Arreglado el leer del main y eliminar librerias que no sirven

--- src/mundo/Main.java
+++ src/mundo/Main.java
@@ -1,28 +1,62 @@
 package mundo;
 
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.ArrayList;
 
 
 
 public class Main {
 
-	private static final String filePath = "./data/archivo.json";
+	private static final String filePath = "./data/archivo.txt";
 
 	public static void main(String[] args)
 	{
+		int clientes = 0;
+        int servidores = 0;
+        int nClientes = 0;
+        int buffer = 0 ;
+		//-----------------------------------------------------
+		// Metodo para leer el archivo
+		//-----------------------------------------------------
+		 String cadena;
+	        FileReader f;
+			try {
+				f = new FileReader(filePath);
+				 BufferedReader b = new BufferedReader(f);
+			        if((cadena = b.readLine())!=null && cadena.contains(":")) {
+			            clientes = Integer.parseInt(cadena.split(":")[1]);
+			        }
+			        if((cadena = b.readLine())!=null && cadena.contains(":")) {
+			            servidores= Integer.parseInt(cadena.split(":")[1]);
+			        }
+			        if((cadena = b.readLine())!=null && cadena.contains(":")) {
+			            nClientes= Integer.parseInt(cadena.split(":")[1]);
+			        }
+			        if((cadena = b.readLine())!=null && cadena.contains(":")) {
+			            buffer= Integer.parseInt(cadena.split(":")[1]);
+			        }
+			        b.close();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				System.out.println("Fallo leyendo el archivo");
+			}
+	        
 		
 		ArrayList<Cliente> listaClie = new ArrayList<Cliente>();
 		ArrayList<Servidor> listaSer = new ArrayList<Servidor>();
 		
-		Buffer  buff = new Buffer(3, 20) ;				
+		Buffer  buff = new Buffer(buffer, 20) ;				
 		
-		for( int j = 0 ; j < 20 ; j++ )
+		for( int j = 0 ; j < clientes ; j++ )
 		{
-			Cliente nuevillo = new Cliente(j, buff, 10) ;
+			Cliente nuevillo = new Cliente(j, buff, nClientes) ;
 			listaClie.add(nuevillo) ;
 		}
-		for( int k = 0 ; k < 4 ; k++ )
+		for( int k = 0 ; k < servidores; k++ )
 		{
 			Servidor nuevilloSer = new Servidor(k, buff);
 			listaSer.add(nuevilloSer) ;
